693246f19c78a551324395f405c8dcf235fc2885
Rust Talapov <rust.talapov@gmail.com>
2018-02-02 17:08:05 +0200

    finish commit

---
+++ src/components/MovieFilter.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="movie-filter">
+        <h2>Filter Results</h2>
+        <h3>By time of day</h3>
+        <div class="filter-group">
+            <check-filter
+                    category="time"
+                    v-for="time in times"
+                    v-bind:title="time"
+            ></check-filter>
+        </div>
+        <h3>By genre</h3>
+        <div class="filter-group">
+            <check-filter
+                    category="genre"
+                    v-for="genre in genres"
+                    v-bind:title="genre"
+            ></check-filter>
+        </div>
+    </div>
+</template>
+
+<script>
+  import genres from '../util/genres';
+  import times from '../util/times';
+  import CheckFilter from './CheckFilter.vue';
+
+  export default {
+    data: function () {
+      return {
+        genres,
+        times
+      }
+    },
+    components: {
+      CheckFilter
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,66 +1,55 @@
 import Vue from 'vue';
 import './style.scss';
-import genres from './util/genres';
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+import moment from 'moment-timezone';
+
+moment.tz.setDefault('UTC');
+Object.defineProperty(Vue.prototype, '$moment', {
+  get() {
+    return this.$root.moment;
+  }
+});
+
+import {checkFilter, setDay} from "./util/bus";
+
+const bus = new Vue();
+Object.defineProperty(Vue.prototype, '$bus', {
+  get() {
+    return this.$root.bus;
+  }
+});
+
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+import routes from './util/routes';
+
+const router = new VueRouter({routes});
+
+import Tooltip from './util/tooltip';
+
+Vue.use(Tooltip);
 
 new Vue({
-    el: '#app',
-    components: {
-        'movie-list': {
-            template:
-                `<div id="movie-list">
-                    <div v-for="movie in movies" class="movie">
-                        {{ movie.title }}
-                    </div>
-                </div>`,
-            data: function () {
-                return {
-                    movies: [
-                        {title: 'Pulp Fiction'},
-                        {title: 'Home Alone'},
-                        {title: 'Austin Powers'}
-                    ]
-                }
-            }
-        },
-        'movie-filter': {
-            data: function () {
-              return {
-                  genres
-              }
-            },
-            template:
-                `<div id="movie-filter">
-                    <h2>Filter Results</h2>
-                    <div class="filter-group">
-                        <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
-                    </div>
-                </div>`,
-            methods: {
-                checkFilter() {
-                    console.log('Check!');
-                }
-            },
-            components: {
-                'check-filter': {
-                    data: function () {
-                        return {
-                            checked: false
-                        }
-                    },
-                    props: ['title'],
-                    template:
-                    `<div v-bind:class="{ 'check-filter': true, 'active': checked }" v-on:click="checkFilter">
-                        <span class="checkbox"></span>
-                        <span class="check-filter-title">{{ title }}</span>
-                    </div>`,
-                    methods: {
-                        checkFilter() {
-                            this.checked = !this.checked;
-                            this.$emit('check-filter');
-                        }
-                    }
-                }
-            }
-        }
-    }
+  el: '#app',
+  data: {
+    genre: [],
+    time: [],
+    movies: [],
+    moment,
+    day: moment(),
+    bus
+  },
+  created() {
+    this.$http.get('/api').then(response => {
+      this.movies = response.data;
+    });
+    this.$bus.$on('check-filter', checkFilter.bind(this));
+    this.$bus.$on('set-day', setDay.bind(this));
+  },
+  router
 });
